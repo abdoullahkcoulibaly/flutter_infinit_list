51b43189077e676a11461aee4678edd1b3177e81
Abdoulaye Coulibaly <148540706+abdoullahkcoulibaly@users.noreply.github.com>
2023-10-27 10:56:00 +0000

    initial

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_infinit_list/posts/views/post_page.dart';
+
+class App extends MaterialApp {
+  const App({super.key}) : super(home: const PostsPage());
+}
